3b9b3679cb4e799415d66fe1b68d7179459a540d
Eduardo Max <eduardomax27@outlook.com>
2023-10-18 08:27:31 -0300

    update: pix e arquivos faltando

--- controller/checkoutController.php
+++ controller/checkoutController.php
@@ -43,13 +43,13 @@ class checkoutController
         // Se o QR Code for para pagamento único (só puder ser utilizado uma vez), descomente a linha a seguir.
         //$px[01]="12"; //Se o valor 12 estiver presente, significa que o BR Code só pode ser utilizado uma vez. 
         $px[26][00]="BR.GOV.BCB.PIX"; //Indica arranjo específico; “00” (GUI) obrigatório e valor fixo: br.gov.bcb.pix
-        $px[26][01]="01858758661"; //Chave do destinatário do pix, pode ser EVP, e-mail, CPF ou CNPJ.
+        $px[26][01]="eduardomax27@outlook.com"; //Chave do destinatário do pix, pode ser EVP, e-mail, CPF ou CNPJ.
 
         $px[52]="0000"; //Merchant Category Code “0000” ou MCC ISO18245
         $px[53]="986"; //Moeda, “986” = BRL: real brasileiro - ISO4217
         $px[54]= $amount; //Valor da transação, se comentado o cliente especifica o valor da transação no próprio app. Utilizar o . como separador decimal. Máximo: 13 caracteres.
         $px[58]="BR"; //“BR” – Código de país ISO3166-1 alpha 2
-        $px[59]="LUCAS JOSE CAMPOS PACHECO"; //Nome do beneficiário/recebedor. Máximo: 25 caracteres.
+        $px[59]="Eduardo Silva"; //Nome do beneficiário/recebedor. Máximo: 25 caracteres.
         $px[60]="BELO HORIZONTE"; //Nome cidade onde é efetuada a transação. Máximo 15 caracteres.
         $px[62][05]="***"; //Identificador de transação, quando gerado automaticamente usar ***. Limite 25 caracteres. Vide nota abaixo.
 
